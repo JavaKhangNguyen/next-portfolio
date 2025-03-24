2a6f4a683d9367f48893b58558bfbb991f18c9f3
Nguyễn Phúc Khang <khang2073@gmail.com>
2025-03-24 14:36:17 +0700

    Update personal data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -10,8 +10,8 @@ export const personalData = {
   linkedIn: 'https://www.linkedin.com/in/ngpkhang/',
   resume: "https://drive.google.com/file/d/1c995VKOZevEF-ljHFgIYArpMvBQel7H2/view?usp=drive_link",
   langs: ["VN", "GB"], // For the langs key, make sure to check the syntax of react-flagkit when inputting data here
-  hirable: 'true',
-  currentEmployment: 'Available',
+  hirable: 'false',
+  currentEmployment: 'DXC Technology Vietnam',
   exp: 'Fresher',
-  fieldsOfInterest: ['Frontend','Backend','Fullstack','AI', 'DA', 'Game Dev']
+  // fieldsOfInterest: ['Frontend','Backend','Fullstack','AI', 'DA', 'Game Dev']
 }
