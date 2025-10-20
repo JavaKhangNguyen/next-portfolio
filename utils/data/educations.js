136d6ca2affdfee2fbf269024373bb46ceeabc48
Nguyễn Phúc Khang <khang2073@gmail.com>
2025-10-20 19:57:35 +0700

    Update education ordering

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,16 +1,16 @@
 export const educations = [
-  {
+   {
     id: 1,
-    title: "Bachelor of Engineering",
-    duration: "09/2020 - 09/2024",
-    institution: "International University - VNU HCM",
-    logo :"/orgs/IU.webp"
-  },
-  {
-    id: 2,
     title: "Data Analyst Specialization",
     duration: "03/2025 - 09/2025",
     institution: "CodeGym",
     logo :"/orgs/CG.webp"
+  },
+  {
+    id: 2,
+    title: "Bachelor of Engineering",
+    duration: "09/2020 - 09/2024",
+    institution: "International University - VNU HCM",
+    logo :"/orgs/IU.webp"
   },
 ]
