ce8fd2b4ff5f1436b53f455de6c496326fcc339e
Nguyễn Phúc Khang <khang2073@gmail.com>
2026-02-27 20:56:55 +0700

    Align build settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from "vite";
 import vinext from "vinext";
 
 export default defineConfig({
-  plugins: [vinext()],
-  build: {
-    outDir: 'build',
-    emptyOutDir: true, 
-  },
+  plugins: [vinext()]
 });
